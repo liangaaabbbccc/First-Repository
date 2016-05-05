f14c7058438dc56f6c47c490eb7fbeb24f6a85c6
梁泽华 <liangaaabbbccc@163.com>
2016-05-05 15:22:21 +0800

    wangyuquan

---
+++ Second GitHub project/Second GitHub project/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Second GitHub project
+//
+//  Created by 梁泽华 on 16/5/5.
+//  Copyright © 2016年 梁泽华. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"hahaha");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
