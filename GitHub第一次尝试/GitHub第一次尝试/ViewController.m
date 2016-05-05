453b6348ec097969006ba658a1bffa8e299f4df2
梁泽华 <liangaaabbbccc@163.com>
2016-05-05 14:50:48 +0800

    第一次改动

--- GitHub第一次尝试/GitHub第一次尝试/ViewController.m
+++ GitHub第一次尝试/GitHub第一次尝试/ViewController.m
@@ -19,6 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"hahaha");
+    NSLog(@"已改动");
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
